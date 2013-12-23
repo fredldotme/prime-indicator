7651696ce95847c346e7cd3a7249ee10fd53c408
Alfred Neumayer <dev.beidl@gmail.com>
2013-12-23 02:15:44 +0100

    add files

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-prime-indicator
-===============
+PRIME Indicator
+==============
+Indicator applet for Ubuntu 14.04 laptop users with NVIDIA/Intel switchable graphics
+This indicator has only been tested on Ubuntu 14.04 but should be working just as well
+on 12.04 with the newest hardware enablement stack.
+Apply Google magic in case you want to use this on a version other than 14.04.
 
-PRIME Indicator - indicator applet for NVIDIA Optimus laptops
+This indicator applet allows owners of laptops with NVIDIA/Intel hybrid graphics to
+quickly switch between the graphics cards and log out from within the Unity top panel in seconds.
+
+
+Prerequisites
+==============
+You have to have a few packages installed on your Ubuntu box to take advantage of this indicator.
+Make sure you have the NVIDIA driver >=331.20 installed and the additional package called "nvidia-prime".
+In case the indicator doesn't start because it is missing the python module "appindicator",
+again, make sure to install missing packages. To install all needed dependencies:
+
+sudo apt-get install nvidia-prime nvidia-331 nvidia-settings python-appindicator
+
+
+How to install
+==============
+
+chmod a+x setup.sh
+sudo ./setup.sh
---
+++ setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# PRIME Indicator - indicator applet for NVIDIA Optimus laptops
+# Copyright (C) 2013 Alfred Neumayer
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+if [[ $EUID -ne 0 ]]; then
+	echo "This script must be run using sudo" 2>&1
+	exit 1
+else
+	mkdir -p /usr/lib/primeindicator
+	cp prime-indicator /usr/bin/
+	chown root:root /usr/bin/prime-indicator
+	chmod 755 /usr/bin/prime-indicator
+	cp igpuon /usr/lib/primeindicator/
+	cp dgpuon /usr/lib/primeindicator/
+	cp intel.png /usr/lib/primeindicator/
+	cp nvidia.png /usr/lib/primeindicator/
+	chmod a+r /usr/lib/primeindicator/*.png
+	chown root:root /usr/lib/primeindicator/igpuon
+	chown root:root /usr/lib/primeindicator/dgpuon
+	chmod a+x /usr/lib/primeindicator/igpuon
+	chmod a+x /usr/lib/primeindicator/dgpuon
+	cp prime-indicator-sudoers /etc/sudoers.d/
+	chmod 644 /etc/sudoers.d/prime-indicator-sudoers
+
+	read -n1 -p "Autostart PRIME Indicator? (y/N) "
+	echo
+	[[ $REPLY = [yY] ]] && cp prime-indicator.desktop $HOME/.config/autostart/ || { rm $HOME/.config/autostart/prime-indicator.desktop; }
+fi
